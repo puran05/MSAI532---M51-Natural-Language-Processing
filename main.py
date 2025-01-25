0805f54487a41865551af80c5f5135854fcc9576
puran05 subedi <isubedipuran@gmail.com>
2025-01-25 18:40:29 +0000

    added pos tagging

--- main.py
+++ main.py
@@ -1,12 +1,21 @@
 import nltk
 from nltk.tokenize import sent_tokenize, word_tokenize
 from nltk.corpus import stopwords
+from nltk.stem import PorterStemmer
 
 nltk.download('punkt')
 nltk.download("stopwords")
+nltk.download('averaged_perceptron_tagger')
+
+stemmer = PorterStemmer()
 
 sentence_break = """
 After the dark comes the sunshine . A new day comes with new promises. Spread love and peace
+"""
+stemming_portion ="""
+The use of technology has dramatically changed how we communicate. 
+Some people find its usefulness in connecting with others, while others argue about its uselessness in fostering meaningful relationships. 
+Still, the user experience varies depending on how one chooses to use it.
 """
 # This is the part to break the sentences
 # sentences = sent_tokenize(sentence_break)
@@ -28,4 +37,11 @@ for word in words:
     if word.casefold() not in stop_words:
         filtered_words.append(word)
 
-print(filtered_words)
+#Stemming the word is the process to reduce the words to their root
+# Below is the snippet for stemming the words
+stemmed_portion = [stemmer.stem(word) for word in words]
+
+# here we have done tagging part of speech , also known as pos tagging, where the text is tagged according to speech.
+tagged_words = nltk.pos_tag(words)
+print(tagged_words)
+
